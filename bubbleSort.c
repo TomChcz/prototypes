094bea22ed8e59560cda1b7bbd2f8923736e9e4f
Tomas Chadzivasilis <chadzicz@gmail.com>
2017-02-13 22:14:55 +0000

    added comment to array size

--- bubbleSort.c
+++ bubbleSort.c
@@ -11,7 +11,7 @@
 int main(void)
 {
     // fill haystack
-    int size = 5;
+    int size = 5; // smaler than haystack size to avoid segmentation fault (out of bounds error)
     int haystack[] = {6, 5, 4, 3, 2, 1};
     
     for(int i = 0; i < size; i++)
