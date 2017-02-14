3291dc09656f0e358dd4f8672e6ab8f6ff7c056f
Tomas Chadzivasilis <chadzicz@gmail.com>
2017-02-14 07:41:02 +0000

    added pseudocode for swap & mark sorted

--- bubbleSort.c
+++ bubbleSort.c
@@ -14,12 +14,19 @@ int main(void)
     int size = 5; // smaler than haystack size to avoid segmentation fault (out of bounds error)
     int haystack[] = {6, 5, 4, 3, 2, 1};
     
+    // look in haystack
     for(int i = 0; i < size; i++)
     {
         // check if following element bigger
         if(haystack[i] > haystack [i + 1])
         {
             printf("ready to swap %i with %i\n", haystack[i], haystack[i + 1]);
+            
+            /*  pseudocode for swap of positions in array
+            haystack[i] = haystack[i+1], haystack[i + 1] = haystack[i]
+            need to figure out, how to mark element as sorted (index at zero and start for loop by size+1?)
+            */
+            
         }
     }
 }
