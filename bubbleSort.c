6d2b4dfb4df2baf17c4feb2188f8a22b9f6e0ce4
Tomas Chadzivasilis <chadzicz@gmail.com>
2017-02-17 23:00:14 +0000

    working prototype for longer array

--- bubbleSort.c
+++ bubbleSort.c
@@ -11,31 +11,33 @@
 int main(void)
 {
     // fill haystack
-    int size = 2; // smaler than haystack size to avoid segmentation fault (out of bounds error)
-    int haystack[] = {6, 2, 5};
+    int size = 6; // smaler than haystack size to avoid segmentation fault (out of bounds error)
+    int haystack[] = {5, 2, 1, 20, 60, 3, 0};
     
     // look in haystack
-    for(int i = 0; i < size; i++)
+    
+    for(int n = 0; n < size; n++)
     {
-        // check if following element bigger
-        if(haystack[i] > haystack [i + 1])
+        for(int i = 0; i < size; i++)
         {
-
-            // swap: need to create a function for this
+            // check if following element bigger
+            if(haystack[i] > haystack [i + 1])
+            {
+                // swap: need to create a function for this
             
-            //store sorted haystack
-            int tmp = haystack[i];
-            // swap straws
-            haystack[i] = haystack[i + 1];
-            haystack[i + 1] = tmp;
-           
+                //store sorted haystack
+                int tmp = haystack[i];
+                // swap straws
+                haystack[i] = haystack[i + 1];
+                haystack[i + 1] = tmp;
 
-            /*  pseudocode for swap of positions in array
-            haystack[i] = haystack[i+1], haystack[i + 1] = haystack[i]
-            need to figure out, how to mark element as sorted (index at zero and start for loop by size+1?)
-            */
-            
+                /*  pseudocode for swap of positions in array
+                haystack[i] = haystack[i+1], haystack[i + 1] = haystack[i]
+                need to figure out, how to mark element as sorted (index at zero and start for loop by size+1?)
+                */
+            }
         }
+        
     }
 
     // print haystack values
@@ -45,7 +47,7 @@ int main(void)
             printf("%i ", haystack[x]);
         }
    
-   // new line
+    // new line
     printf("\n");
     
     
