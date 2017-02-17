f67c3159811f4eda22dc950025e8923dc030d70a
Tomas Chadzivasilis <chadzicz@gmail.com>
2017-02-17 17:59:56 +0000

    first working sort for ideal array

--- bubbleSort.c
+++ bubbleSort.c
@@ -11,8 +11,8 @@
 int main(void)
 {
     // fill haystack
-    int size = 1; // smaler than haystack size to avoid segmentation fault (out of bounds error)
-    int haystack[] = {6, 2};
+    int size = 2; // smaler than haystack size to avoid segmentation fault (out of bounds error)
+    int haystack[] = {6, 2, 5};
     
     // look in haystack
     for(int i = 0; i < size; i++)
@@ -20,29 +20,33 @@ int main(void)
         // check if following element bigger
         if(haystack[i] > haystack [i + 1])
         {
-            printf("pass nr %i ready to swap %i with %i\n", i, haystack[i], haystack[i + 1]);
-            
-            // need to create a function for this
+
+            // swap: need to create a function for this
             
             //store sorted haystack
             int tmp = haystack[i];
             // swap straws
             haystack[i] = haystack[i + 1];
             haystack[i + 1] = tmp;
+           
 
-       
             /*  pseudocode for swap of positions in array
             haystack[i] = haystack[i+1], haystack[i + 1] = haystack[i]
             need to figure out, how to mark element as sorted (index at zero and start for loop by size+1?)
             */
             
         }
-        
-        // print haystack values
-        for(int x = 0; x < size + 1; x++)
-        {
-            printf("array index %i holds value of %i\n", x, haystack[x]);
-        }
-        
     }
+
+    // print haystack values
+    printf("sorted haystack: ");
+    for(int x = 0; x < size + 1; x++)
+        {
+            printf("%i ", haystack[x]);
+        }
+   
+   // new line
+    printf("\n");
+    
+    
 }
