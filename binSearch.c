72c4639c97932f7c2fafe584513dec751114ebeb
Tomas Chadzivasilis <chadzicz@gmail.com>
2017-02-21 08:39:23 +0000

    first working prototype

--- binSearch.c
+++ binSearch.c
@@ -18,62 +18,68 @@ int main(int argc, string argv[])
         return 1;
     }
     
+    // get needle
+    int needle = atoi(argv[1]);
+    
     /* pseudocode
      * look at midpoint (modulus/2 -1 for even array size), if match exit, if higher, look to the left, if lower, look to the right
      * repat until array portion of size 1
+     * end is higher than start
      */
      
     // fil the haystack
-    int size = 7;
-    int haystack[] = {1, 2, 3, 4, 5, 6, 7};
-    // int haystack[] = {1, 2, 3, 4, 5, 6};
+    int haystack[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    // define midpoint variable
+    int midpoint;
     
-    // define counter for recursion
-    int counter;
-     
-    // get needle
-    int needle = atoi(argv[1]);
+    // define start of the array
+    int start = 0;
+    
+    // define end of the array (size of array - 1)
+    int end = 14;
+
     printf("needle is %i\n", needle);
-     
+
     do
     {
-        counter = 0;
-
-        int midpoint;
-         
-        //find midpoint
-        if(size % DIVIDER == 1)
-        {
-            // define midpoint and substract 1 to get array position
-            midpoint = ((size - 1) / DIVIDER) - 1;
-            printf("haystack is of odd size\n");
-        }
-        else
-        {
-            // define midpoint and substract 1 to get array position
-            midpoint = (size / DIVIDER) -1;
-            printf("haystack is of even size\n");
-        }
-         
+        
+    //calculate midpoint
+    if((start + end) % DIVIDER == 1)
+    {
+        // define midpoint for odd size haystack and substract 1 to get array position
+        midpoint = ((start + end - 1) / DIVIDER);
+        printf("midpoint is %i\n", midpoint);
+    }
+    else
+    {
+        // define midpoint for even size haystack and substract 1 to get array position
+        midpoint = ((start + end) / DIVIDER);
+        printf("midpoint is %i\n", midpoint);
+    }        
         // search the haystack
         if(haystack[midpoint] == needle)
         {
-            printf("needle found!\n");
+            printf("needle found! at index %i\n", midpoint);
+            return 0;
         }
         else
         {
-            // counter++;
-            if(haystack[midpoint] > needle)
+            if(needle < haystack[midpoint])
             {
                 // TODO look to the left
-                printf("look left\n");
+                end = midpoint - 1;
+                printf("new start is %i end is %i\n", start, end);
             }
             else
             {
                 // TODO look to the right
-                printf("look right\n");
+                start = midpoint + 1;
+                printf("new start is %i and end is %i\n", start, end);
             }
         } 
     }
-    while(counter > 0);
+    while(end >= start);
+    
+    printf("needle not found :(\n");
 }
