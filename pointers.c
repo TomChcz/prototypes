12c1a80f0ff9a4e3a8023df74971515c447f46f7
Tomas Chadzivasilis <chadzicz@gmail.com>
2017-03-16 22:40:24 +0000

    more fun

--- pointers.c
+++ pointers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -13,7 +14,6 @@ int main(void)
     // print length (size) of a string
     printf("string length is: %lu\n", strlen(s));
     
-
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         // go to address and print value (this time char), increments i every iteration and moves to next byte in string
@@ -21,9 +21,26 @@ int main(void)
     }
     
     // show me where the string begins in memory
-    printf("memory address of the string is %p\n", &s);
+    printf("memory address of the string is %p\n", s);
     
+    int k = 5;
+    
+    printf("memory address of k is: %p\n\n", &k);
+    
+    int* pk = NULL; // create a pointer
+    pk = &k; // put the addess of k in pk
+    
+    // show me where the pointers point to and address of k in memory
+    printf("memory adress of pk is %p and value at the address is %i and int k is %i and memory address of k is %p\n", pk, *pk, k, &k);
+    
+    int* b = malloc(sizeof(int));
 
+    if(b == NULL)
+    {
+        printf("NULL\n\n");
+    }
+    
+    printf("int value is %i and memory adress is %p\n", *b, &b);
     
     
 }
