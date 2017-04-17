eb0851333b889df52e46217fda8f56fcb4d4ac54
Tomas Chadzivasilis <chadzicz@gmail.com>
2017-04-17 18:03:20 +0000

    optimized commit

---
+++ sllist4.c
@@ -0,0 +1,285 @@
+/****************************************************
+ *                                                  *
+ * Singly linked list in c, dynamic version         *
+ *                                                  *
+ * usage ./sllist4                                  *
+ *                                                  *
+ ****************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// define list node
+typedef struct sllist
+{
+    int value; // value of the node
+    struct sllist* next; // pointer to the next node
+}
+sllnode;
+
+// function prototypes
+sllnode* create(int y);
+sllnode* insert(sllnode* head, int n);
+bool search(sllnode* head, int n);
+void print(sllnode* head);
+sllnode* destroy(sllnode* head);
+
+/* create pointer to the first node as a global var to be used in entire program
+initialize at NULL since points to nothing at this time */
+sllnode* root = NULL;
+
+int main(void)
+{
+    // menu variable
+    int choice = 0;
+    
+    // placeholdder
+    int switch_value;
+    
+    // display main menu until terminated by user
+    while(choice !=6)
+    {
+        printf("\n####################################################\n");
+        printf("# Singly Linked List creator, please choose        #\n");
+        printf("#                                                  #\n");
+        printf("# 1. Create list                                   #\n");
+        printf("# 2. Insert node to the list                       #\n");
+        printf("# 3. Search the list                               #\n");
+        printf("# 4. Print the list                                #\n");
+        printf("# 5. Delete the list                               #\n");
+        printf("# 6. Exit                                          #\n");
+        printf("#                                                  #\n");
+        printf("####################################################\n");
+        printf("\nYour choice: ");
+        
+        scanf("%d", &choice);
+        
+        switch(choice)
+        {
+            case 1:
+                printf("\nPlease provide value for the first node: ");                
+                
+                // get user input
+                scanf("%d", &switch_value);
+                
+                // create the list (first node in the list)
+                root = create(switch_value);
+    
+                // make sure list was created
+                if(root != NULL)
+                {
+                    printf("\nFirst node created sucesfully\n\n");
+                }
+                else
+                {
+                    return 1;
+                }
+                break;
+                
+            case 2:
+                printf("\nPlease enter value for another node: ");
+                scanf("%d", &switch_value);
+                
+                // insert new node to the list
+                root = insert(root, switch_value);
+                break;            
+           
+            case 3:
+                printf("\nWhat do you want to find: ");
+                scanf("%d", &switch_value);
+                
+                // search the list
+                search(root, switch_value);
+                break;
+
+            case 4:
+                // print list
+                print(root);
+                break;
+                
+            case 5:
+                // delete list
+                root = destroy(root);
+                break;
+            
+            case 6:
+                printf("\nThanks for using, shutting down ...\n");
+                
+                // check if list exists
+                if(root != NULL)
+                {
+                    // delete list and free up memory
+                    root = destroy(root);
+                }
+                exit(0);
+                break;
+        }
+    // end choice menu
+    }
+
+return 0;
+// end main
+}
+
+// creates first node
+sllnode* create(int y)
+{
+    // complete check if list already exists, need to pass root value, and execute only if NULL aka does not exist
+    // allocate space for new node
+    sllnode* head = malloc(sizeof(sllnode));
+    
+    // check if memory allocated ok
+    if(head == NULL)
+    {
+        fprintf(stderr, "\nCannot create first node (memory issue)\n");
+        return NULL;
+    }
+    else
+    {
+        // add value to new node
+        head->value = y;
+    
+        // add value to the pointer
+        head->next = NULL;
+        
+        // return pointer to first node
+        return head;
+    }
+    
+// end create   
+}
+
+// this inserts new node right after root node (first)
+sllnode* insert(sllnode* head, int n)
+{
+    // check if list exists
+    if(head == NULL)
+    {
+        printf("\nList not found, create list first\n");
+        return NULL;
+    }
+    else
+    {
+        // create empty node in memory
+        sllnode* current = malloc(sizeof(sllnode));
+        
+        // check for memory errors
+        if(current == NULL)
+        {
+            fprintf(stderr, "\nMemory for node cant be allocated\n");
+            return NULL;
+        }
+
+        // add value to the current node
+        current->value = n;
+        
+        // pointer now points where head points
+        current->next = head;
+
+        // link the head to link to following node (or null)
+        head = current;
+        
+        //success
+        printf("\nNode with value %i created successfully\n\n", n);
+        
+        return current;
+    }
+
+// end insert
+}
+
+// searches the list for value
+bool search(sllnode* head, int n)
+{
+    if(head == NULL)
+    {
+        printf("\nList does not exist, create list first\n");
+        return false;
+    }
+    else
+    {
+        // replicate root node links
+        sllnode* trav = head;
+        
+        // search the list
+        while(trav != NULL)
+        {
+            if(trav->value == n) // if found, return sucess
+            {
+                printf("\n%i found in the list\n", n);
+                return true;
+            }
+            else // else move to next node and repeat
+            {
+                trav = trav->next;
+            }
+        }
+        printf("\n%i not found in the list\n", n);
+        return false; // return false after not found in entire list
+    }
+
+// end search
+}
+
+// prints the entire list
+void print(sllnode* head)
+{
+    if(head == NULL)
+    {
+        printf("\nList does not exist, create list first\n");
+    }
+    else
+    {
+        // create traverse node
+        sllnode* trav = head;
+        
+        // node counter
+        int counter = 1;
+        printf("\nList summary\n");        
+        while(trav != NULL)
+        {
+            //print node contents
+
+            printf("Node %i address: %p\n", counter, trav);
+            printf("Node %i value is: %i\n", counter, trav->value);
+            printf("Node %i ->: %p\n\n", counter, trav->next);
+            
+            // move to next node
+            trav = trav->next;
+            
+            // increment counter
+            counter ++;
+        }
+    }
+    
+// end print
+}
+
+// deletes entire list
+sllnode* destroy(sllnode* head)
+{
+    // check if list exists
+    if(head == NULL)
+    {
+        printf("\nList does not exist, cant delete\n");
+        return NULL;
+    }
+    else
+    {
+        // recursively loop until last node
+        if(head->next != NULL)
+        {
+            // delete rest of the list
+            destroy(head->next);
+        }
+    
+        // reset pointer to next node
+        head->next = NULL;
+        // free current node
+        free(head);
+        return NULL;
+    }
+    
+// end destroy
+}
